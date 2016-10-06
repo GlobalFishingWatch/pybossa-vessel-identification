9d01af9ce6ccc3915705249361b1a1549cd78e72
bitsofbits <tim@bitsofbits.com>
2016-10-06 06:31:02 -0700

    lay some groundwork for automating; no time to finish now

--- id_fishing_points/results/scripts/query_cogsog_tracks.py
+++ id_fishing_points/results/scripts/query_cogsog_tracks.py
@@ -1,18 +1,19 @@
 import sys
 import json
+import subprocess
 
 dataset_name = sys.argv[1]
 
 with open("%s_tasks.json" % dataset_name) as f:
     mmsis = set((task["info"]["mmsi"] for task in json.load(f)))
 
-print """
+query = """
 SELECT
   lat, lon, speed, course, mmsi, timestamp
 FROM
   TABLE_DATE_RANGE([pipeline_normalize.], TIMESTAMP('2015-01-01'), TIMESTAMP('2015-12-31')) 
 where
-  mmsi in (%s)
+  mmsi in ({0})
   and lat is not null
   and lon is not null
   and timestamp is not null
@@ -20,4 +21,21 @@ where
   and lat<90 and lat>-90 and lon<180 and lon>-180 and lon !=0 and lat !=0
 order by
   mmsi, timestamp
-""" % ",".join(mmsis)
+""".format(",".join(mmsis))
+
+print(query)
+
+# print(subprocess.check_call(["bq", "query", query]))
+
+
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     import argparse
+    
