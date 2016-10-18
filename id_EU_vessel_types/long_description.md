c0eb99b9c178f13681d59c1a7b981310300294ef
David Kroodsma <davidkroodsma@gmail.com>
2016-10-18 13:00:01 -0700

    added a few projects

---
+++ id_EU_vessel_types/long_description.md
@@ -0,0 +1,6 @@
+Thank you for helping Global Fishing Watch identify!
+
+We and the fish thank you!
+
+* * *
+
---
+++ id_eastern_pacific/creating_tasks/4_create_input_file.py
@@ -0,0 +1,53 @@
+from os import listdir
+from os.path import isfile, join
+import random
+from operator import itemgetter
+import csv
+
+today_date = "20160907"
+mypath = "../../data/vessels_"+today_date+"/"
+mypath = "../../data/vessels_"+today_date+"/"
+files = [f.replace(".json","") for f in listdir(mypath) if isfile(join(mypath, f)) and ".json" in f]
+
+
+vessels_doubled = [f.split("_")[0] for f in files]
+vessels = []
+for v in vessels_doubled:
+    if v not in vessels:
+        vessels.append(v)
+
+tasks = []
+
+for v in vessels:
+    selects = []
+    selects2 = []
+    #v_months[v]=[]
+    for f in files:
+        if str(v) == f.split("_")[0]:
+            m = int(f.split("_")[2])
+            y = int(f.split("_")[1])
+            if y == 2015:
+                selects.append(m)
+            if y == 2014:
+                selects2.append(m)
+    if len(selects)+len(selects2)>2:
+        # selects = sorted(selects, key=itemgetter(0))
+        selects = sorted(selects)#, key=itemgetter(1))
+        selects2 = sorted(selects2)#, key=itemgetter(1))
+
+        for i in range(len(selects)): 
+            selects[i]=str(selects[i])
+        
+        for j in range(len(selects2)): 
+            selects2[j]=str(selects2[j])
+
+#        print str(v)+"\t"+",".join(selects2)+"\t"+ (",".join(selects))
+        tasks.append([v,",".join(selects)])
+
+
+
+with open("../tasks_"+today_date+".csv", 'wb') as f:
+    writer = csv.writer(f)
+    theheader = ['mmsi','months_2015']
+    writer.writerow(theheader) 
+    writer.writerows(tasks)
