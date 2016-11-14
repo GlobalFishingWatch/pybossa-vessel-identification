6c10cd63ad7d123bf61b390e1066a90d1ca4ae47
bitsofbits <tim@bitsofbits.com>
2016-11-14 12:41:03 -0700

    add inferred accuracy

--- id_fishing_points/results/scripts/evaluate.py
+++ id_fishing_points/results/scripts/evaluate.py
@@ -24,6 +24,47 @@ from mussidae import time_range_tools as trtools
 import download_tracks
 from collections import defaultdict
 
+
+def inferred_accuracy(agreement):
+    """Convert agreement to an equivalent accuracy
+
+    Args:
+        agreement: float
+            Fraction of pairs of raters who agree with each other. 
+            In range [0, 1]
+
+    Returns:
+        accuracy: float
+
+    Note that this doesn't return the REAL accuracy in terms of fishing / non-fishing,
+    rather it assumes that there is some standard that all the raters are trying to 
+    meet (transiting / non-fishing or such) and tries to infer how accuracte
+    individual rating are by examing how ofter different raters agree.
+
+    Assuming that the raters return the true correct answer with a 
+    a probability `alpha` (the accuracy) then `a` (the agreement)
+    can be determined by:
+    ```
+        a = alpha * alpha + (1 - alpha) * (1 - alpha)
+    ```
+    In other words the agreement is the fraction of the time both
+    raters are correct plus the fraction of the time both raters
+    are wrong. This leads to:
+    ```
+        a = 1 - 2 * alpha + 2 * alpha ** 2 
+        => (1 - a) - 2 * alpha + 2 * alpha ** 2 = 0
+        => alpha = (2 +- sqrt(4 - 8 * (1 - a)) / 4
+                 = 0.5 * (1 + sqrt(1 - 2 * (1 - a)))  # Assume accuracy > 0.5
+                 ~ 0.5 * (1 + 1 - (1 - a))            # for (1 - a) << 1
+                 = 1 - (1 - a) / 2
+    ```
+    We use the exact formula here, but the final formula is of interest in that it shows
+    that the `error` (`1 - alpha`) is half the `disagreement` (`1 - a`).
+
+    """
+    return 0.5 * (1 + np.sqrt(1 - 2 * (1 - agreement)))
+
+
 def compare_users(dataset_name, n_worst, class_map):
 
     with open("%s_tasks.json" % dataset_name) as f:
@@ -40,7 +81,7 @@ def compare_users(dataset_name, n_worst, class_map):
 
     gear_map = {}
     all_mmsi = set()
-
+    task_count = 0
 
     for tidx, task in enumerate(tasks.values()):
         gear_map[task['info']['mmsi']] = task['info']['vesselType']
@@ -63,9 +104,11 @@ def compare_users(dataset_name, n_worst, class_map):
         task_task_runs = [task_run for task_run in task_runs.values()
                           if task_run["task_id"] == task["id"]]
 
-        if len(task_task_runs) < 3:
-            # Only look at tasks that at least three people have looked at
+        if len(task_task_runs) < 2:
+            # Only look at tasks that at least two people have looked at
             continue
+
+        task_count += 1
 
         values = []
 
@@ -98,6 +141,9 @@ def compare_users(dataset_name, n_worst, class_map):
 
             values.append(is_fishing)
 
+        if not values:
+            continue
+
         values = np.transpose(values)
         #
         all_mmsi.add(task["info"]["mmsi"])
@@ -118,15 +164,16 @@ def compare_users(dataset_name, n_worst, class_map):
         #
         avg_std_dev = std_devs.mean() if len(std_devs) else 0
 
-        # Also compute the agreement. Agreement with more than 2 people is computed as
-        # the fraction of all possible pairs that agree == a * (a - 1) / (n * (n - 1))
+        # Also compute the agreement. We compute the number of pairs that agree as well as the number of
+        # total pairs. We then compute the overall agreement, by dividing these later on.
 
         values[isnan] = 0
         max_count = values.shape[1]
         a = values.sum(axis=1)
         b = (n - a)
         # Pairwise agreement
-        agreement = (a * (a - 1) + b * (b - 1)) / (n * (n - 1.0)) 
+        agreement = a * (a - 1) + b * (b - 1)
+        pairs = n * (n - 1.0)
         # Consensus agreement  
         consensus = np.where(a > b, a, b)
         # We consider it a consensus if more than half of the raters agree
@@ -135,33 +182,38 @@ def compare_users(dataset_name, n_worst, class_map):
         consensus_aggr = np.where(has_consenseus, (consensus / n), 0)
         assert np.all(a <= n), (a, b, n)
 
-        avg_agreement = agreement.mean() if len(agreement) else 0
+        pairs_count = pairs.sum() if len(pairs) else 0
+        avg_agreement = agreement.sum() / pairs_count if pairs_count else 0
         avg_cons_aggr = consensus_aggr.mean() if len(consensus_aggr) else 0
 
 
         #
-        std_dev_map[task["info"]["mmsi"]].append((avg_std_dev, avg_agreement, avg_cons_aggr, len(std_devs)))
+        std_dev_map[task["info"]["mmsi"]].append((avg_std_dev, avg_agreement, avg_cons_aggr, len(std_devs), pairs_count))
 
         #
     std_dev_for_mmsi = {}
     agreement_for_mmsi = {}
     cons_aggreement_for_mmsi = {}
     count_for_mmsi = {}
+    pairs_count_for_mmsi = {}
     for m in std_dev_map:
         total_std = 0
         total_agr = 0
         total_cons = 0
         count = 0
-        for std, agr, cons, cnt in std_dev_map[m]:
+        pairs_count = 0
+        for std, agr, cons, cnt, pcnt in std_dev_map[m]:
             total_std += cnt * std
-            total_agr += cnt * agr
+            total_agr += pcnt * agr
             total_cons += cnt * cons
+            pairs_count += pcnt
             count += cnt
         if count:
             std_dev_for_mmsi[m] = total_std / count
-            agreement_for_mmsi[m] = total_agr / count
+            agreement_for_mmsi[m] = total_agr / pairs_count
             cons_aggreement_for_mmsi[m] = total_cons / count
             count_for_mmsi[m] = count
+            pairs_count_for_mmsi[m] = pairs_count
         #
 
     mmsi = sorted(std_dev_for_mmsi, key=lambda x: std_dev_for_mmsi[x])
@@ -172,40 +224,50 @@ def compare_users(dataset_name, n_worst, class_map):
             print(m, std_dev_for_mmsi[m], agreement_for_mmsi[m], gear_map[m])
         print()
 
+    print("Total number of Tasks with enough raters", task_count)
+    print("Total number of Tasks", len(tasks))
     print("Total number of MMSI processed", len(all_mmsi))
     print("Total number of MMSI processed by more than one person", len(agreement_for_mmsi))
     # print("Mean stddev over MMSI: ", np.mean(std_dev_for_mmsi.values()))
     count = 0
+    pairs_count = 0
     total_aggr = 0
     total_cons_aggr = 0
     total_stddev = 0
     for m in agreement_for_mmsi:
-        total_aggr += count_for_mmsi[m] * agreement_for_mmsi[m]
+        total_aggr += pairs_count_for_mmsi[m] * agreement_for_mmsi[m]
         total_cons_aggr += count_for_mmsi[m] * cons_aggreement_for_mmsi[m]
         total_stddev += count_for_mmsi[m] * std_dev_for_mmsi[m]
 
         count += count_for_mmsi[m]
+        pairs_count += pairs_count_for_mmsi[m]
 
-    print("Pairwise agreement over all MMSI", total_aggr / count)
+    agreement = total_aggr / pairs_count
+    print("Pairwise agreement over all MMSI", agreement)
+    print("Inferred accuracy over all MMSI", inferred_accuracy(agreement))
     print("Consensus agreement over all MMSI", total_cons_aggr / count)
     print("Standard Deviationn over all MMSI", total_stddev / count)
     print()
 
     for cls in np.unique(class_map.values()):
         count = 0
+        pairs_count = 0
         total_aggr = 0
         total_cons = 0
         total_std = 0
         for m in agreement_for_mmsi:
             if class_map.get(int(m)) == cls:
-                total_aggr += count_for_mmsi[m] * agreement_for_mmsi[m]
+                total_aggr += pairs_count_for_mmsi[m] * agreement_for_mmsi[m]
                 total_cons += count_for_mmsi[m] * cons_aggreement_for_mmsi[m]
                 total_std += count_for_mmsi[m] * std_dev_for_mmsi[m]
                 count += count_for_mmsi[m]
+                pairs_count += pairs_count_for_mmsi[m]
         if count:
-            print("Pairwise agreement for", cls, total_aggr / count)
+            agreement = total_aggr / pairs_count
+            print("Pairwise agreement for", cls, agreement)
+            print("Inferred accuracy for", cls, inferred_accuracy(agreement))
             print("Consensus agreement for", cls, total_cons / count)
-            print("Standard Deviationn for", cls, total_std / count)
+            print("Standard Deviation for", cls, total_std / count)
             print()
 
 
